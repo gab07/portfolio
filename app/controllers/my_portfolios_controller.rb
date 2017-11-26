71beae46462873eaf0c971dea3dba055c6004455
Gabriel Aguirre <gabrielaguirre921@gmail.com>
2017-11-26 18:57:45 -0500

    added edit and updated actions for portfolios

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -9,8 +9,7 @@ class MyPortfoliosController < ApplicationController
   end
 
   def create
-    @item = MyPortfolio.create!(params
-                                    .require(:my_portfolio).permit(:title, :subtitle, :body))
+    @item = MyPortfolio.create!(params.require(:my_portfolio).permit(:title, :subtitle, :body))
     respond_to do |format|
       if @item.save
         format.html { redirect_to my_portfolios_path, notice: 'Item created.'}
@@ -20,5 +19,20 @@ class MyPortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = MyPortfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = MyPortfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:my_portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to my_portfolios_path, notice: 'Item created.'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
 end
