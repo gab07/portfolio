1f78447ba2c23db3f0bd2319cc10fa58b26fa9a9
Gabriel Aguirre <gabrielaguirre921@gmail.com>
2017-12-02 13:57:13 -0500

    renamed portfolio table to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :my_portfolios
-  get 'pages/home'
+  resources :projects
 
   get 'pages/about'
-
   get 'pages/contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,8 @@ end
 puts "5 skills created"
 
 9.times do |portfolio_item|
-  MyPortfolio.create!(
-               title: "Portfolio title: #{portfolio_item}",
+  Project.create!(
+               title: "Project title: #{portfolio_item}",
                subtitle: "My great service",
                body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                main_image: "http://via.placeholder.com/600x400",
@@ -26,4 +26,4 @@ puts "5 skills created"
   )
 end
 
-puts "9 portfolio items created"
+puts "9 projects created"
