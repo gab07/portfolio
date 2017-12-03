08e6f8754170ce72ccfc8ef964a53aae544817bd
Gabriel Aguirre <gabrielaguirre921@gmail.com>
2017-12-03 13:52:51 -0500

    Implemented concerns and updated seeds

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
+  include Placeholder
+  after_initialize :set_defaults
+
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 
 end
